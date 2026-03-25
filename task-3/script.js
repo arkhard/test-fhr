48013b8ae00b1fa55f53bc66e746e4cc5346a3ab
GeorgyUruckoev <georgy.uruckoev@onlinesup.com>
2026-03-25 23:27:06 +0300

    fix: lint

--- task-3/script.js
+++ task-3/script.js
@@ -1,10 +1,9 @@
-let renderScoreboard;
-const stateScoreboard = { left: 0, right: 0 };
+let renderScoreboard
+const stateScoreboard = { left: 0, right: 0 }
 
 function changeScore(side, date) {
-  console.log(stateScoreboard)
-  stateScoreboard[side] = Math.max(0, stateScoreboard[side] + date);
-  renderScoreboard();
+  stateScoreboard[side] = Math.max(0, stateScoreboard[side] + date)
+  renderScoreboard()
 }
 
 function initButtons() {
@@ -12,7 +11,7 @@ function initButtons() {
   const leftButtonMinus = document.getElementById('left-minus')
   const rightButtonPlus = document.getElementById('right-plus')
   const rightButtonMinus = document.getElementById('right-minus')
-  const resetButton = document.getElementById('reset-button');
+  const resetButton = document.getElementById('reset-button')
 
   leftButtonPlus.addEventListener('click', () => changeScore('left', 1))
   leftButtonMinus.addEventListener('click', () => changeScore('left', -1))
@@ -22,51 +21,51 @@ function initButtons() {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  initButtons();
-  renderScoreboard = initRender();
-  renderScoreboard();
+  initButtons()
+  renderScoreboard = initRender()
+  renderScoreboard()
 })
 
 function resetScores() {
-  stateScoreboard.left = 0;
-  stateScoreboard.right = 0;
-  renderScoreboard();
+  stateScoreboard.left = 0
+  stateScoreboard.right = 0
+  renderScoreboard()
 }
 
 function initRender() {
-  const status = document.getElementById('status');
-  const leftScore = document.getElementById('left-score');
-  const rightScore = document.getElementById('right-score');
-  const leftMinus = document.getElementById('left-minus');
-  const rightMinus = document.getElementById('right-minus');
-  const leftTeam = document.getElementById('leftTeam');
-  const rightTeam = document.getElementById('rightTeam');
+  const status = document.getElementById('status')
+  const leftScore = document.getElementById('left-score')
+  const rightScore = document.getElementById('right-score')
+  const leftMinus = document.getElementById('left-minus')
+  const rightMinus = document.getElementById('right-minus')
+  const leftTeam = document.getElementById('leftTeam')
+  const rightTeam = document.getElementById('rightTeam')
 
   return function() {
-    leftScore.textContent = stateScoreboard.left;
-    rightScore.textContent = stateScoreboard.right;
+    leftScore.textContent = stateScoreboard.left
+    rightScore.textContent = stateScoreboard.right
   
-    leftMinus.disabled = stateScoreboard.left === 0;
-    rightMinus.disabled = stateScoreboard.right === 0;
+    leftMinus.disabled = stateScoreboard.left === 0
+    rightMinus.disabled = stateScoreboard.right === 0
 
-    leftTeam.classList.remove('leading', 'tie');
-    rightTeam.classList.remove('leading', 'tie');
-    status.classList.remove('leading-text');
+    leftTeam.classList.remove('leading', 'tie')
+    rightTeam.classList.remove('leading', 'tie')
+    status.classList.remove('leading-text')
 
     if (stateScoreboard.left === stateScoreboard.right) {
-      leftTeam.classList.add('tie');
-      rightTeam.classList.add('tie');
-      status.textContent = 'Ничья';
+      leftTeam.classList.add('tie')
+      rightTeam.classList.add('tie')
+      status.textContent = 'Ничья'
     } else if (stateScoreboard.left > stateScoreboard.right) {
-      leftTeam.classList.add('leading');
-      rightTeam.classList.add('tie');
-      status.classList.add('leading-text');
-      status.textContent = `Ведут: ${status.dataset.leftTeam}`;
+      leftTeam.classList.add('leading')
+      rightTeam.classList.add('tie')
+      status.classList.add('leading-text')
+      status.textContent = `Ведут: ${status.dataset.leftTeam}`
     } else {
-      leftTeam.classList.add('tie');
-      rightTeam.classList.add('leading');
-      status.classList.add('leading-text');
-      status.textContent = `Ведут: ${status.dataset.rightTeam}`;
+      leftTeam.classList.add('tie')
+      rightTeam.classList.add('leading')
+      status.classList.add('leading-text')
+      status.textContent = `Ведут: ${status.dataset.rightTeam}`
     }
   }
 }
